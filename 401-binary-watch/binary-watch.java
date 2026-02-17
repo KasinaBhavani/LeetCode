d7e88404877e9bc22d6dca01c6f1ad51a5ae4378
KasinaBhavani <kasinabhavani.2005@gmail.com>
2026-02-17 22:58:45 +0530

    Time: 4 ms (89.09%) | Memory: 49.3 MB (42.24%) - LeetSync

---
+++ 401-binary-watch/binary-watch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> res = new ArrayList<>();
+        for(int i=0;i<12;i++){
+            for(int j=0;j<60;j++){
+                if(Integer.bitCount(i)+Integer.bitCount(j)==turnedOn){
+                    if(j<10){
+                        res.add(i+":0"+j);
+                    }
+                    else{
+                        res.add(i+":"+j);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
