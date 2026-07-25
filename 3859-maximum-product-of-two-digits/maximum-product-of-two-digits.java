6ea6e8de4099e92a17d64a25205d6f8915f7bf6b
KasinaBhavani <kasinabhavani.2005@gmail.com>
2026-07-25 07:46:03 +0530

    Time: 1 ms (100.00%) | Memory: 42.6 MB (58.74%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProduct(int n) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        boolean a1 = false;
+        boolean a2 = false;
+        while (n > 0) {
+            int t = n % 10;
+            n /= 10;
+            if (!a1) {
+                a1 = true;
+                max1 = t;
+            }
+            else if (a1 && !a2) {
+                a2 = true;
+                max2 = t;
+            }
+            else if (t >= max1 || t >= max2) {
+                max2 = Math.max(max2, max1);
+                max1 = t;
+            } 
+        }
+        return max1 * max2;
+    }
+}
