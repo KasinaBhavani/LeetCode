a69f6760fe325f33b8c2277574a3304c425c5470
KasinaBhavani <kasinabhavani.2005@gmail.com>
2026-08-05 06:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (53.17%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,18 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null || head.next==null){
-            return false;
-        }
-        ListNode s =head;
-        ListNode f= head;
-        while(f!=null && f.next!=null){
-            s=s.next;
-            f=f.next.next;
-            if(s==f){
-                return true;
-            }
-        }
-        return false;
+       ListNode slow=head;
+       ListNode fast=head;
+       while(fast!=null && fast.next!=null)
+       {
+        slow=slow.next;
+        fast=fast.next.next;
+        if(slow==fast) return true;
+       } 
+       return false;
     }
 }
